dd9f318aa5c09111a2afb599494f8303833d50ee
imran-kkz <imranrali1@gmail.com>
2020-06-23 17:23:27 -0400

    edit

--- fin_django/settings.py
+++ fin_django/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
